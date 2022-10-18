19df0054117f584752776199c8468d49625f6b36
Nicholas Christensen <njchris2@illinois.edu>
2022-10-17 19:10:27 -0700

    only test 3to2 kernels

--- test_fused_autotuning.py
+++ test_fused_autotuning.py
@@ -7,7 +7,7 @@ import os
 from os.path import exists
 from utils import unique_program_id
 
-use_charm=False
+use_charm=True
 if use_charm:
     from charm4py import entry_method, chare, Chare, Array, Reducer, Future, charm
     from charm4py.pool import PoolScheduler, Pool
@@ -818,7 +818,7 @@ def autotune_standalone_subkernels(tunits, save_path=None):
                             autotune_standalone_subkernel(sk, queue, program_id=pid, max_flop_rate=clpeak_flop_rate,
                                     device_latency=device_latency, device_memory_bandwidth=device_memory_bandwidth, save_path=save_path)
 
-                        elif not indirection and red_axes > 0 and total_axes <= 4 and einsum_count <= 2:
+                        elif not indirection and red_axes > 0 and total_axes <= 3 and einsum_count <= 4:
                             autotune_standalone_subkernel(sk, queue, program_id=pid, max_flop_rate=clpeak_flop_rate,
                                     device_latency=device_latency, device_memory_bandwidth=device_memory_bandwidth, save_path=save_path)
 
