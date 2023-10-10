f30d8401a063d8bb0c5b3bbbeb79330dd2e9fbea
Nicholas Christensen <njchris2@illinois.edu>
2023-10-10 01:10:42 -0500

    Always emit new evaluation file

--- tagtune/ytopt_autotuning.py
+++ tagtune/ytopt_autotuning.py
@@ -301,7 +301,7 @@ def ytopt_tuning(in_queue, knl, platform_id, input_space, program_id=None, max_f
                 # results.
                 hjson_file_str = save_path + "/" + pid + ".hjson"
 
-                if not exists(hjson_file_str) or pre_existing_evals < max_evals:
+                if True:#not exists(hjson_file_str) or pre_existing_evals < max_evals:
                     tdict = run_single_param_set_v2(in_queue, knl, trans_list, generic_test,
                                 max_flop_rate=max_flop_rate,
                                 device_memory_bandwidth=device_memory_bandwidth,
@@ -318,7 +318,7 @@ def ytopt_tuning(in_queue, knl, platform_id, input_space, program_id=None, max_f
 
                 if True: # See what the performance is with the full kernel.
                     hjson_file_str = save_path + "/" + pid + "_full" + ".hjson"
-                    if not exists(hjson_file_str) or pre_existing_evals < max_evals:
+                    if True:#not exists(hjson_file_str) or pre_existing_evals < max_evals:
                         tdict = run_single_param_set_v2(in_queue, knl, trans_list, generic_test,
                                     max_flop_rate=max_flop_rate,
                                     device_memory_bandwidth=device_memory_bandwidth,
